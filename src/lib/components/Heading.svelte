8102185ba6eb48a706e3bb2adf455fd58ab91756
Altanbagana Erdene-Ochir <altanbagana@protonmail.com>
2026-07-19 07:56:05 +0800

    chore: cleaning up

--- src/lib/components/Heading.svelte
+++
@@ -1,5 +0,0 @@
-<p
-	class="w-full text-center text-4xl lg:text-8xl font-display font-black tracking-widest uppercase mb-14 lg:mb-20 text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-500"
->
-	<slot />
-</p>
